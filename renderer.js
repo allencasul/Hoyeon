a593f7ca93c28c85599509d0c330ad6ee253f2b6
Allen Charls Casul <120450666+allencasul@users.noreply.github.com>
2023-04-28 22:56:04 -0700

    Delete renderer.js

--- renderer.js
+++
@@ -1,7 +0,0 @@
-const text = document.getElementById('txt');
-const button = document.getElementById('btn');
-
-button.addEventListener('click', () => {
-  text.textContent = "Powered by ElectronJS";
-  button.textContent = "Clicked";
-});
